74ef3249a572e5dc13455a081c116821fbccc9c5
Alex Iraola <alex.iraola@gmail.com>
2023-12-19 00:52:22 +0100

    Block test

--- src/tests/game.spec.ts
+++ src/tests/game.spec.ts
@@ -1,21 +1,36 @@
 class GameOfLife {
-  private table: number[][];
-
-  constructor(private readonly width: number, private readonly height: number) {
-    this.table = new Array(width).fill(new Array(height).fill(0));
-  }
+  constructor(private table: number[][]) { }
 
   status() {
     return this.table;
+  }
+
+  nextGeneration() {
+    this.table = this.table.map(rows => {
+      return rows.map(column => {
+        return column;
+      });
+    });
   }
 }
 
 describe('Game of life', () => {
   it('creates a game table', () => {
-    const game = new GameOfLife(20, 20);
+    const game = new GameOfLife([[0, 0, 0], [0, 0, 0], [0, 0, 0]]);
 
     const table = game.status();
 
-    expect(table).toStrictEqual(new Array(20).fill(new Array(20).fill(0)))
+    expect(table).toStrictEqual(new Array(3).fill(new Array(3).fill(0)))
+  });
+
+  it('generates a correctly a block', () => {
+    const block = [[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]];
+    const game = new GameOfLife(block);
+
+    expect(game.status()).toStrictEqual(block);
+
+    game.nextGeneration();
+
+    expect(game.status()).toStrictEqual(block);
   });
 });
