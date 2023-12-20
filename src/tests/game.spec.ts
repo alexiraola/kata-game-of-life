7db812dbf6e6d5f330b4488421f8b2f6b1d60b64
Alex Iraola <alex.iraola@gmail.com>
2023-12-20 19:08:51 +0100

    Move to its own file

--- src/tests/game.spec.ts
+++ src/tests/game.spec.ts
@@ -1,48 +1,4 @@
-import { Cell } from "../core/cell";
-
-class GameOfLife {
-  private constructor(private table: Cell[][]) { }
-
-  static createWithInitialState(state: number[][]) {
-    return new GameOfLife(state.map(row => row.map(cell => Cell.createFromNumber(cell))));
-  }
-
-  status() {
-    return this.table.map(row => row.map(cell => cell.isLive() ? 1 : 0));
-  }
-
-  tick() {
-    this.table = this.table.map((rows, row) => {
-      return rows.map((cell, column) => {
-        return cell.nextCell(this.liveNeighbours(row, column));
-      });
-    });
-  }
-
-  private liveNeighbours(row: number, column: number) {
-    return this.neighbours(row, column).filter(cell => cell.isLive()).length;
-  }
-
-  private neighbours(row: number, column: number) {
-    const maxIndex = this.table.length - 1;
-
-    const left = column === 0 ? maxIndex : column - 1;
-    const top = row === 0 ? maxIndex : row - 1;
-    const right = column === maxIndex ? 0 : column + 1;
-    const bottom = row === maxIndex ? 0 : row + 1;
-
-    return [
-      this.table[top][left],
-      this.table[top][column],
-      this.table[top][right],
-      this.table[row][left],
-      this.table[row][right],
-      this.table[bottom][left],
-      this.table[bottom][column],
-      this.table[bottom][right]
-    ];
-  }
-}
+import { GameOfLife } from '../core/game';
 
 describe('Game of life', () => {
   it('creates a game table', () => {
