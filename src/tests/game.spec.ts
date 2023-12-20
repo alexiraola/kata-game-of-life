82f1f40ab126f2c92adf29d3b19480c6db52b9c7
Alex Iraola <alex.iraola@gmail.com>
2023-12-20 18:52:51 +0100

    Generates correctly a blinker

--- src/tests/game.spec.ts
+++ src/tests/game.spec.ts
@@ -12,17 +12,40 @@ class GameOfLife {
   }
 
   tick() {
-    this.table = this.table.map(rows => {
-      return rows.map(column => {
-        return column;
+    this.table = this.table.map((rows, row) => {
+      return rows.map((cell, column) => {
+        return cell.nextCell(this.liveNeighbours(row, column));
       });
     });
+  }
+
+  private liveNeighbours(row: number, column: number) {
+    return this.neighbours(row, column).filter(cell => cell.isLive()).length;
+  }
+
+  private neighbours(row: number, column: number) {
+    const maxIndex = this.table.length - 1;
+
+    const topLeft = row === 0 || column === 0 ? Cell.createDead() : this.table[row - 1][column - 1];
+    const top = row === 0 ? Cell.createDead() : this.table[row - 1][column];
+    const topRight = row === 0 || column === maxIndex ? Cell.createDead() : this.table[row - 1][column + 1];
+    const left = column === 0 ? Cell.createDead() : this.table[row][column - 1];
+    const right = column === maxIndex ? Cell.createDead() : this.table[row][column + 1];
+    const bottomLeft = row === maxIndex || column === 0 ? Cell.createDead() : this.table[row + 1][column - 1];
+    const bottom = row === maxIndex ? Cell.createDead() : this.table[row + 1][column];
+    const bottomRight = row === maxIndex || column === maxIndex ? Cell.createDead() : this.table[row + 1][column + 1];
+
+    return [topLeft, top, topRight, left, right, bottomLeft, bottom, bottomRight];
   }
 }
 
 describe('Game of life', () => {
   it('creates a game table', () => {
-    const game = GameOfLife.createWithInitialState([[0, 0, 0], [0, 0, 0], [0, 0, 0]]);
+    const game = GameOfLife.createWithInitialState([
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+    ]);
 
     const table = game.status();
 
@@ -30,7 +53,12 @@ describe('Game of life', () => {
   });
 
   it('generates a correctly a block', () => {
-    const block = [[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]];
+    const block = [
+      [0, 0, 0, 0],
+      [0, 1, 1, 0],
+      [0, 1, 1, 0],
+      [0, 0, 0, 0]
+    ];
     const game = GameOfLife.createWithInitialState(block);
 
     expect(game.status()).toStrictEqual(block);
@@ -38,5 +66,30 @@ describe('Game of life', () => {
     game.tick();
 
     expect(game.status()).toStrictEqual(block);
+  });
+
+  it('generates correctly a blinker', () => {
+    const blinker1 = [
+      [0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0],
+      [0, 0, 1, 0, 0],
+      [0, 0, 1, 0, 0],
+      [0, 0, 0, 0, 0]
+    ];
+    const blinker2 = [
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 1, 1, 1, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0]
+    ];
+    const game = GameOfLife.createWithInitialState(blinker1);
+
+    expect(game.status()).toStrictEqual(blinker1);
+
+    game.tick();
+
+
+    expect(game.status()).toStrictEqual(blinker2);
   });
 });
