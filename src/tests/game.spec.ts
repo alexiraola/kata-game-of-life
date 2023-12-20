ae48796c41c57b6895eb4a527474c451ce05bf30
Alex Iraola <alex.iraola@gmail.com>
2023-12-20 23:54:46 +0100

    Generates correctly a beacon

--- src/tests/game.spec.ts
+++ src/tests/game.spec.ts
@@ -50,7 +50,6 @@ describe('Game of life', () => {
 
     game.tick();
 
-
     expect(game.status()).toStrictEqual(blinker2);
   });
 
@@ -77,7 +76,94 @@ describe('Game of life', () => {
 
     game.tick();
 
-
     expect(game.status()).toStrictEqual(toad2);
+  });
+
+  it('generates correctly a beacon', () => {
+    const beacon1 = [
+      [0, 0, 0, 0, 0, 0],
+      [0, 1, 1, 0, 0, 0],
+      [0, 1, 1, 0, 0, 0],
+      [0, 0, 0, 1, 1, 0],
+      [0, 0, 0, 1, 1, 0],
+      [0, 0, 0, 0, 0, 0]
+    ];
+    const beacon2 = [
+      [0, 0, 0, 0, 0, 0],
+      [0, 1, 1, 0, 0, 0],
+      [0, 1, 0, 0, 0, 0],
+      [0, 0, 0, 0, 1, 0],
+      [0, 0, 0, 1, 1, 0],
+      [0, 0, 0, 0, 0, 0]
+    ];
+    const game = GameOfLife.createWithInitialState(beacon1);
+
+    expect(game.status()).toStrictEqual(beacon1);
+
+    game.tick();
+
+    expect(game.status()).toStrictEqual(beacon2);
+  });
+
+  it('generates correctly a glider', () => {
+    const glider1 = [
+      [0, 0, 1, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0],
+      [0, 1, 1, 1, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0]
+    ];
+    const glider2 = [
+      [0, 0, 0, 0, 0, 0],
+      [0, 1, 0, 1, 0, 0],
+      [0, 0, 1, 1, 0, 0],
+      [0, 0, 1, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0]
+    ];
+    const glider3 = [
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0],
+      [0, 1, 0, 1, 0, 0],
+      [0, 0, 1, 1, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0]
+    ];
+    const glider4 = [
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 0],
+      [0, 0, 0, 1, 1, 0],
+      [0, 0, 1, 1, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0]
+    ];
+    const glider5 = [
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0],
+      [0, 0, 0, 0, 1, 0],
+      [0, 0, 1, 1, 1, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0]
+    ];
+    const game = GameOfLife.createWithInitialState(glider1);
+
+    expect(game.status()).toStrictEqual(glider1);
+
+    game.tick();
+
+    expect(game.status()).toStrictEqual(glider2);
+
+    game.tick();
+
+    expect(game.status()).toStrictEqual(glider3);
+
+    game.tick();
+
+    expect(game.status()).toStrictEqual(glider4);
+
+    game.tick();
+
+    expect(game.status()).toStrictEqual(glider5);
   });
 });
