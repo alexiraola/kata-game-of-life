e8ee370a9f3b806cf6e4b2fee41cf2221c44b0d3
Alex Iraola <alex.iraola@gmail.com>
2023-12-20 19:02:04 +0100

    Refactor neighbours

--- src/tests/game.spec.ts
+++ src/tests/game.spec.ts
@@ -26,16 +26,21 @@ class GameOfLife {
   private neighbours(row: number, column: number) {
     const maxIndex = this.table.length - 1;
 
-    const topLeft = row === 0 || column === 0 ? Cell.createDead() : this.table[row - 1][column - 1];
-    const top = row === 0 ? Cell.createDead() : this.table[row - 1][column];
-    const topRight = row === 0 || column === maxIndex ? Cell.createDead() : this.table[row - 1][column + 1];
-    const left = column === 0 ? Cell.createDead() : this.table[row][column - 1];
-    const right = column === maxIndex ? Cell.createDead() : this.table[row][column + 1];
-    const bottomLeft = row === maxIndex || column === 0 ? Cell.createDead() : this.table[row + 1][column - 1];
-    const bottom = row === maxIndex ? Cell.createDead() : this.table[row + 1][column];
-    const bottomRight = row === maxIndex || column === maxIndex ? Cell.createDead() : this.table[row + 1][column + 1];
+    const left = column === 0 ? maxIndex : column - 1;
+    const top = row === 0 ? maxIndex : row - 1;
+    const right = column === maxIndex ? 0 : column + 1;
+    const bottom = row === maxIndex ? 0 : row + 1;
 
-    return [topLeft, top, topRight, left, right, bottomLeft, bottom, bottomRight];
+    return [
+      this.table[top][left],
+      this.table[top][column],
+      this.table[top][right],
+      this.table[row][left],
+      this.table[row][right],
+      this.table[bottom][left],
+      this.table[bottom][column],
+      this.table[bottom][right]
+    ];
   }
 }
 
